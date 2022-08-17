03608bb91c1002649e3edda87fd1adebab625bb5
Ilya Fomin <ilyafomin9283@mail.ru>
2022-08-17 23:31:32 +0300

    + about & services sections

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,9 +5,36 @@ const Header = () => {
   return (
     <div className="header">
       <ul>
-        <li>Главная</li>
-        <li>Обо мне</li>
-        <li>Услуги</li>
+        <li
+          onClick={() =>
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth",
+            })
+          }
+        >
+          Главная
+        </li>
+        <li
+          onClick={() =>
+            window.scrollTo({
+              top: 750,
+              behavior: "smooth",
+            })
+          }
+        >
+          Обо мне
+        </li>
+        <li
+          onClick={() =>
+            window.scrollTo({
+              top: 1500,
+              behavior: "smooth",
+            })
+          }
+        >
+          Услуги
+        </li>
         <li>Отзывы</li>
         <li>Вопросы</li>
       </ul>
